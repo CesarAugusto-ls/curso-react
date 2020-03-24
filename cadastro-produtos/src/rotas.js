b992bb659c95f36c0da6a79003609541ccd417e7
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-03-24 20:57:45 -0300

    rotas

--- cadastro-produtos/src/rotas.js
+++ cadastro-produtos/src/rotas.js
@@ -3,6 +3,7 @@ import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './views/home'
 import CadastroProduto from './views/produtos/cadastro'
+import ConsultaProdutos from './views/produtos/consulta'
 
 export default () => {
     return (
@@ -11,6 +12,7 @@ export default () => {
                 <Route exact path="/" component={Home} />
 
                 <Route exact path="/cadastroprodutos" component={CadastroProduto} />
+                <Route exact path="/consultaprodutos" component={ConsultaProdutos} />
             </Switch>
         </HashRouter>
     )
