53fdebf5f91576fb316cdb13b7d2eb8d01391723
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-03-29 17:30:05 -0300

    useEffetc

--- react-funcional/src/App.js
+++ react-funcional/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [numero, setNumero] = useState()
-  const [segundoNumero, setSegundoNumero] = useState()
-  const [resultado, setRsultado] = useState()
+  const [numero, setNumero] = useState('')
+  const [segundoNumero, setSegundoNumero] = useState('')
+  const [resultado, setRsultado] = useState('')
 
   // const [state, setState] = useState({
   //   nuemro: 0,
@@ -19,15 +19,19 @@ function App() {
     setRsultado(numeroInt + segundoNumeroInt)
   }
 
+  useEffect(() => {
+    console.log('variavel numero: ', numero)
+  })
+
   return (
     <div className="App">
       Numero 1:<br />
-      <input type="text" value={numero} onChange={e => setNumero(e.target.value)} /><br/>
+      <input type="text" value={numero} onChange={e => setNumero(e.target.value)} /><br />
       Numero 2:<br />
-      <input type="text" value={segundoNumero} onChange={e => setSegundoNumero(e.target.value)} /><br/>
+      <input type="text" value={segundoNumero} onChange={e => setSegundoNumero(e.target.value)} /><br />
       <button onClick={somar}>somar</button><br />
       Resultado:<br />
-      <input type="text" value={resultado}/><br/>
+      <input type="text" value={resultado} /><br />
     </div>
   );
 }
