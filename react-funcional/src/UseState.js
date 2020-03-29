6dab1f79c99b9abecd241ec8200e7ff3cd724c76
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-03-29 17:29:38 -0300

    refatoracao

---
+++ react-funcional/src/UseState.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function UseState() {
+
+  const [numero, setNumero] = useState()
+  const [segundoNumero, setSegundoNumero] = useState()
+  const [resultado, setRsultado] = useState()
+
+  // const [state, setState] = useState({
+  //   nuemro: 0,
+  //   segundoNumero: 0,
+  //   resultado: 0
+  // })
+
+  const somar = () => {
+    const numeroInt = parseInt(numero)
+    const segundoNumeroInt = parseInt(segundoNumero)
+
+    setRsultado(numeroInt + segundoNumeroInt)
+  }
+
+  return (
+    <div className="App">
+      Numero 1:<br />
+      <input type="text" value={numero} onChange={e => setNumero(e.target.value)} /><br/>
+      Numero 2:<br />
+      <input type="text" value={segundoNumero} onChange={e => setSegundoNumero(e.target.value)} /><br/>
+      <button onClick={somar}>somar</button><br />
+      Resultado:<br />
+      <input type="text" value={resultado}/><br/>
+    </div>
+  );
+}
+
+export default UseState;
